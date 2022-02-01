3b1f5f7a3c28753ceec62b371d4baa1903f3994b
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2022-02-01 15:18:58 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Gallery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Gallery
+    {
+        public Gallery()
+        {
+            GalleryImage = new List<string>();
+        }
+
+        [Key]
+        public int GalleryID { get; set; }
+
+        [StringLength(100)]
+        public List<string> GalleryImage { get; set; }
+        
+    }
+}
---
+++ MvcSozenCit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcSozenCit.MvcApplication" Language="C#" %>
